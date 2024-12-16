ade1647dfadf65de05759f64c14e842c52ad8984
Nikolay <koddek@bk.ru>
2024-12-16 23:06:54 +0300

    Refactoring

--- FileManager/FileSystemModule/Controller/FileSystemViewController.swift
+++ FileManager/FileSystemModule/Controller/FileSystemViewController.swift
@@ -10,52 +10,35 @@ import Combine
 
 class FileSystemViewController: UIViewController {
     
+    private lazy var fileSystemView = FileSystemView()
     private let viewModel = FileSystemViewModel()
     private var cancellables = Set<AnyCancellable>()
     
-    private lazy var fileSystemView: FileSystemView = {
-        let view = FileSystemView()
-        view.fileSystemViewdelegate = self
-        return view
-    }()
+    override func loadView() {
+        self.view = fileSystemView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupViews()
-        setupBindings()
         setupNavigationBar()
-        setupConstraints()
-        
-        // Установка начальной директории
-        guard let rootDirectory = viewModel.fileManager.showRootDirectoryContents()?.appendingPathComponent(viewModel.userManager.getUsername() ?? "") else { return }
-        viewModel.currentDirectory = rootDirectory
-    }
-    
-    private func setupViews() {
-        view.addSubview(fileSystemView)
-    }
-    
-    private func setupBindings() {
-        viewModel.$contents
-            .receive(on: RunLoop.main)
-            .sink { [weak self] contents in
-                self?.fileSystemView.reloadData()
-            }
-            .store(in: &cancellables)
-        
-        viewModel.$isAtRootDirectory
-            .receive(on: RunLoop.main)
-            .sink { [weak self] isAtRoot in
-                self?.updateLeftBarButtonItem(isAtRoot: isAtRoot)
-            }
-            .store(in: &cancellables)
+        bindViewModel()
     }
     
     private func setupNavigationBar() {
         navigationItem.rightBarButtonItems = [
-            UIBarButtonItem(image: UIImage(systemName: "folder"), style: .done, target: self, action: #selector(addFolderButtonTapped)),
-            UIBarButtonItem(image: UIImage(systemName: "doc.text"), style: .done, target: self, action: #selector(addFileButtonTapped))
+            UIBarButtonItem(
+                image: UIImage(systemName: "folder"),
+                style: .done,
+                target: self,
+                action: #selector(addFolderButtonTapped)
+            ),
+            UIBarButtonItem(
+                image: UIImage(systemName: "doc.text"),
+                style: .done,
+                target: self,
+                action: #selector(addFileButtonTapped)
+            )
         ]
     }
     
@@ -92,40 +75,11 @@ class FileSystemViewController: UIViewController {
     }
 }
 
-//MARK: - FileSystemViewDelegate
-
-extension FileSystemViewController: FileSystemViewDelegate {
-    
-    func didSelectDirectory(at indexPath: IndexPath) {
-        guard let directory = viewModel.contents[safe: indexPath.row] else { return }
-        viewModel.currentDirectory = directory
-    }
-    
-    func deleteItem(at indexPath: IndexPath) {
-        guard let item = viewModel.contents[safe: indexPath.row] else { return }
-        viewModel.deleteItem(at: item)
-    }
-    
-    func didSelectFile(at url: URL) {
-        let message = viewModel.fileManager.loadTextFromFile(at: url)
-        AlertManager.shared.showTextInsideFileAlert(from: self, at: url, message: message) { text in
-            if let text = text {
-                self.viewModel.fileManager.saveTextToFile(text: text, at: url)
-            }
-        }
-    }
-}
-
-//MARK: - Constraints
+// MARK: - Bind ViewModel
 
 private extension FileSystemViewController {
     
-    func setupConstraints() {
-        NSLayoutConstraint.activate([
-            fileSystemView.topAnchor.constraint(equalTo: view.topAnchor),
-            fileSystemView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            fileSystemView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            fileSystemView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
+    func bindViewModel() {
+       
     }
 }
--- FileManager/FileSystemModule/View/FileSystemView.swift
+++ FileManager/FileSystemModule/View/FileSystemView.swift
@@ -7,56 +7,60 @@
 
 import UIKit
 
-protocol FileSystemViewDelegate: AnyObject {
-    func didSelectFile(at url: URL)
-    func deleteItem(at indexPath: IndexPath)
-    func didSelectDirectory(at indexPath: IndexPath)
-}
-
-class FileSystemView: UITableView {
+class FileSystemView: UIView {
     
-    private let cellIdentifier = "Cell"
-    weak var fileSystemViewdelegate: FileSystemViewDelegate?
+    private let cellIdentifier = "cell"
     
-    override init(frame: CGRect, style: UITableView.Style) {
-        super.init(frame: .zero, style: .insetGrouped)
+    private let tableView = UITableView(
+        frame: .zero,
+        style: .insetGrouped
+    )
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
         setupTableView()
-    }
-    
-    private func setupTableView() {
-        translatesAutoresizingMaskIntoConstraints = false
-        register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
-        dataSource = self
-        delegate = self
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private func setupTableView() {
+        tableView.register(
+            UITableViewCell.self,
+            forCellReuseIdentifier: cellIdentifier
+        )
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
 }
 
 //MARK: - UITableViewDataSource
+
 extension FileSystemView: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        0 
+    
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        0
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: cellIdentifier,
+            for: indexPath
+        )
         return cell
     }
 }
 
 //MARK: - UITableViewDelegate
+
 extension FileSystemView: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        deselectRow(at: indexPath, animated: true)
-        fileSystemViewdelegate?.didSelectDirectory(at: indexPath)
-    }
     
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            fileSystemViewdelegate?.deleteItem(at: indexPath)
-        }
-    }
 }
